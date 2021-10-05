d3c106204241e7fb0178773f46bfec449caee5fc
Cameron Paul <cpaul37@gmail.com>
2021-10-05 18:38:44 -0500

    WIP

---
+++ src/command/mod.rs
@@ -0,0 +1,57 @@
+pub const INFO: &'static str = "info";
+
+pub fn info(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn ls(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn tree(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn cat(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn cp(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn rm(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn mv(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn format(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn boot(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn create(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn partition(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn get(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn put(options: Option<&[String]>) -> i32 {
+    return 0;
+}
+
+pub fn sys(options: Option<&[String]>) -> i32 {
+    return 0;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{App, AppSettings, Arg, SubCommand};
+
+mod command;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Fizzy")
+        .version(VERSION)
+        .about("Read and modify old disk images")
+        .setting(AppSettings::VersionlessSubcommands)
+        .subcommand(
+            SubCommand::with_name("info")
+                .about("controls testing features")
+                .arg(
+                    Arg::with_name("image")
+                        .required(true)
+                        .help("print debug information verbosely"),
+                )
+                .arg(Arg::with_name("partition").required(false).help("foo")),
+        )
+        .get_matches();
+
+    println!("{:?}", matches);
 }
